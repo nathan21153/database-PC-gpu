be1dd4d8d107feac63547020ae0564bf384834a6
nathan21153 <164109270+nathan21153@users.noreply.github.com>
2024-06-20 10:24:57 +1200

    re-uploaded assesment files into GitHub because i had trouble committing through pycharm
    added data, to data base, worked of info page.

--- gpu-db-ass/app.py
+++ gpu-db-ass/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main_page():  # put application's code here
+    return render_template('main-page.html')
 
+@app.route('/info-page')
+def info_page():  # put application's code here
+    return render_template('info-page.html')
+
+@app.route('/gpu-page')
+def gpu_page():  # put application's code here
+    return render_template('gpu-page.html')
 
 if __name__ == '__main__':
     app.run()
